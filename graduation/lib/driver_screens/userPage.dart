fa8d6b536d22fbde3c74d8de4204ed2096b05177
Beyhan Kandemir <beyhankandemir@Beyhan-MacBook-Air.local>
2025-04-21 01:06:34 +0300

    user page design changed make more efficient

--- graduation/lib/driver_screens/userPage.dart
+++ graduation/lib/driver_screens/userPage.dart
@@ -4,7 +4,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:graduation/driver_screens/driver_trips_page.dart';
 import 'package:graduation/services/database.dart';
 import 'package:graduation/models/userInfo.dart';
-import 'package:graduation/userIcon.dart';
 import 'package:graduation/driver_screens/detection_details.dart';
 import '../userDetail.dart';
 
@@ -12,7 +11,7 @@ class UserPage extends StatefulWidget {
   const UserPage({super.key});
 
   @override
-  _UserPageState createState() => _UserPageState();
+  State<UserPage> createState() => _UserPageState();
 }
 
 class _UserPageState extends State<UserPage> {
@@ -31,161 +30,169 @@ class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("User Page")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: FutureBuilder<Driver?>(
-          future: userDataFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return const Center(child: Text("Error loading user info"));
-            } else if (!snapshot.hasData || snapshot.data == null) {
-              return const Center(child: Text("No user data found"));
-            }
+      backgroundColor: Colors.grey.shade900,
+      appBar: AppBar(
+        title: const Text("User Page"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: FutureBuilder<Driver?>(
+        future: userDataFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator(color: Colors.white));
+          } else if (snapshot.hasError || !snapshot.hasData) {
+            return const Center(child: Text("No user data", style: TextStyle(color: Colors.white70)));
+          }
 
-            final user = snapshot.data!;
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                _buildUserCard(context, user),
-                const SizedBox(height: 20.0),
-                _buildRouteContainer(context, user),
-                const SizedBox(height: 20.0),
-                _buildDetectionContainer(context),
-              ],
+          final user = snapshot.data!;
+          return Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildProfileCard(user),
+                  const SizedBox(height: 30),
+                  _buildActionColumn(user),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildProfileCard(Driver user) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.grey.shade800,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: const [BoxShadow(color: Colors.black38, blurRadius: 8)],
+      ),
+      child: Column(
+        children: [
+          const CircleAvatar(
+            radius: 36,
+            backgroundColor: Colors.white12,
+            child: Icon(Icons.person, color: Colors.white, size: 34),
+          ),
+          const SizedBox(height: 14),
+          Text(
+            "${user.firstName} ${user.lastName}",
+            style: const TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 6),
+          Text("Age: ${user.age}", style: const TextStyle(color: Colors.white70, fontSize: 16)),
+          const SizedBox(height: 16),
+          OutlinedButton.icon(
+            onPressed: () async {
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => UserDetail(uid: uid)),
+              );
+              if (result == true) {
+                setState(() {
+                  userDataFuture = dbService.getDriverData();
+                });
+              }
+            },
+            icon: const Icon(Icons.info_outline, color: Colors.white),
+            label: const Text(
+              "Details",
+              style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            style: OutlinedButton.styleFrom(
+              side: const BorderSide(color: Colors.white60),
+              foregroundColor: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionColumn(Driver user) {
+    return Column(
+      children: [
+        _buildActionCard(
+          icon: FontAwesomeIcons.route,
+          label: "Routes",
+          color: Colors.blueAccent,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DriverTripsPage(
+                  driverId: uid,
+                  driverName: "${user.firstName} ${user.lastName}",
+                  onlyView: false,
+                ),
+              ),
             );
           },
         ),
-      ),
+        const SizedBox(height: 20),
+        _buildActionCard(
+          icon: FontAwesomeIcons.triangleExclamation,
+          label: "Detections",
+          color: Colors.deepOrange,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ViolationDetailsPage(
+                  userDb: UserDatabaseService(uid: uid),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 
-  Widget _buildUserCard(BuildContext context, Driver user) {
-    return Container(
-      height: 180,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.grey.shade700,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: const [
-          BoxShadow(color: Colors.black26, blurRadius: 5, offset: Offset(2, 2)),
-        ],
-      ),
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: UserIcon(icon: FontAwesomeIcons.person),
-          ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
+  Widget _buildActionCard({
+    required IconData icon,
+    required String label,
+    required Color color,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 130,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: const [BoxShadow(color: Colors.black45, blurRadius: 6)],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(width: 20),
+            Row(
               children: [
-                Text("Name: ${user.firstName}", style: const TextStyle(color: Colors.white, fontSize: 20)),
-                Text("Surname: ${user.lastName}", style: const TextStyle(color: Colors.white, fontSize: 20)),
-                Text("Age: ${user.age}", style: const TextStyle(color: Colors.white, fontSize: 20)),
-                ElevatedButton(
-                  onPressed: () async {
-                    final result = await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => UserDetail(uid: uid),
-                      ),
-                    );
-                    if (result == true) {
-                      setState(() {
-                        userDataFuture = dbService.getDriverData();
-                      });
-                    }
-                  },
-                  child: const Text('Details'),
+                Icon(icon, size: 36, color: Colors.white),
+                const SizedBox(width: 16),
+                Text(
+                  label,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildRouteContainer(BuildContext context, Driver user) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => DriverTripsPage(
-              driverId: uid,
-              driverName: "${user.firstName} ${user.lastName}", onlyView: false,
-            ),
-          ),
-        );
-      },
-      child: Container(
-        height: 180,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.blue.shade800,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(color: Colors.black26, blurRadius: 5, offset: Offset(2, 2)),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.directions_car, size: 40, color: Colors.black54),
-            const SizedBox(height: 10),
-            const Text(
-              "Route",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
-            ),
-            const SizedBox(height: 10),
-            const Icon(Icons.arrow_forward_ios, color: Colors.black54),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildDetectionContainer(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ViolationDetailsPage(
-              userDb: UserDatabaseService(uid: uid),
-            ),
-          ),
-        );
-      },
-      child: Container(
-        height: 180,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.red.shade800,
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: const [
-            BoxShadow(color: Colors.black26, blurRadius: 5, offset: Offset(2, 2)),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.warning, size: 40, color: Colors.black54),
-            const SizedBox(height: 10),
-            const Text(
-              "Detections",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
-            ),
-            const SizedBox(height: 10),
-            const Icon(Icons.arrow_forward_ios, color: Colors.black54),
+            const Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Icon(Icons.arrow_forward_ios, size: 16, color: Colors.white70),
+            )
           ],
         ),
       ),
